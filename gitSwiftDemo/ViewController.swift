aea10c411e3c99698132a0cbab020c6e5e38d3d9
yiwugou_mac <clg_dream@163.com>
2018-11-16 17:07:10 +0800

    one commit

---
+++ gitSwiftDemo/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  gitSwiftDemo
+//
+//  Created by yiwugou_mac on 2018/11/16.
+//  Copyright © 2018年 yiwugou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("ddd")
+        let a = "ddd"
+        let b = a
+        print(b)
+        
+    }
+
+    @IBAction func clickBtnAction(_ sender: UIButton) {
+        
+    }
+    
+}
+
